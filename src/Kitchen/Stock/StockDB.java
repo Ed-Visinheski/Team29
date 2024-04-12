4112c46e4f4eb9918a141a41c6759b6cbda48b62
ChenJiangyi2003 <jiangyi.chen@city.ac.uk>
2024-04-12 14:04:36 +0100

    stock 1

--- src/Kitchen/Stock/StockDB.java
+++ src/Kitchen/Stock/StockDB.java
@@ -1,5 +1,48 @@
 package Kitchen.Stock;
 
-public class StockDB {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class StockDB {
+    private Connection connection;
+
+    public StockDB() {
+        Connect();
+    }
+    public void Connect() {
+        String url = "jdbc:mysql://smcse-stuproj00.city.ac.uk:3306/in2033t29";
+        String user = "in2033t29_a";
+        String password = "NvG2lCOEy_g";
+
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection successful");
+        } catch (SQLException e) {
+            System.err.println("Error connecting to the database: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void Disconnect() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+                System.out.println("Disconnected from the database");
+            }
+        } catch (SQLException e) {
+            System.err.println("Error closing connection: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void main(String[] args) {
+        StockDB connector = new StockDB();
+        connector.Connect();
+        // launch(args);
+    }
 }
