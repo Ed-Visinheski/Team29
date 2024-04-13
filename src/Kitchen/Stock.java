aee2f6b21ef1bca37fdb5210236e40de8b77404f
ChenJiangyi2003 <jiangyi.chen@city.ac.uk>
2024-04-13 17:26:15 +0100

    stock gui form

---
+++ src/Kitchen/Stock.java
@@ -0,0 +1,20 @@
+package Kitchen;
+
+import javax.swing.*;
+
+public class Stock {
+    private JPanel StockPanel;
+    private JTextField textIngredientID;
+    private JTextField textStockLevel;
+    private JTextField textStockThreshold;
+    private JTextField textDeliveryArrivalDate;
+    private JButton addButton;
+    private JButton updateButton;
+    private JButton saveButton;
+    private JButton deleteButton;
+    private JTable table1;
+    private JLabel img;
+    private JTextField textSearch;
+    private JButton searchButton;
+    private JPanel Menu;
+}
