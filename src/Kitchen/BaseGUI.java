f6f926dcae33f1fc8eb93285e125052a7724f50c
VithujaThangarajah <vithuja.thangarajah@city.ac.uk>
2024-04-14 20:32:46 +0100

    BaseGUI update

--- src/Kitchen/BaseGUI.java
+++ src/Kitchen/BaseGUI.java
@@ -2,9 +2,22 @@ package Kitchen;
 
 import javax.swing.*;
 import java.awt.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class BaseGUI extends JFrame {
-    private JPanel panel1;
+    private JPanel BaseGUIPanel;
+    private JPanel MenuPanel;
+    private JLabel logo;
+    private JButton dashboardButton;
+    private JButton menuManagementButton;
+    private JButton StockManagementButton;
+    private JButton WasteManagementButton;
+    private JButton ordersAndServicesButton;
+    private JButton settingsButton;
+    private JButton signOutButton;
 
     public BaseGUI(){
         ImageIcon img = new ImageIcon("src/Kitchen/Img/Lancaster.jpeg");
